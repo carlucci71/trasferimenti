fd85ba8c7e4959dbb3a3e3dd5a22a7c94aa8ccda
Daniele <carlucci.daniele@gmail.com>
2024-12-07 08:54:42 +0000

    update

--- lancia.sh
+++ lancia.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+java_pid=$(ps aux | grep it.daniele.transfermarket.TransfermarketApplication | grep spring | awk '!/awk/ {print $2}')
+
+if [ -z "$java_pid" ]; then
+    echo "Nessun processo Java trovato con il nome: $process_name"
+else
+    echo "Terminazione del processo Java con PID: $java_pid"
+    kill -9 "$java_pid"
+fi
+
+
 nohup mvn clean spring-boot:run &
 tail -f nohup.out
 
